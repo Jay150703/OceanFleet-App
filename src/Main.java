4392f07ab918e19cceb3d71b4d39b82d67ac42b6
Jay150703 <jayanthijemi15@gmail.com>
2026-02-04 11:17:55 +0530

    Modified src

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import com.jayanthi.oceanfleet.model.Vessel;
+import com.jayanthi.oceanfleet.util.VesselUtil;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Project Started");
+
+        VesselUtil util = new VesselUtil();
+
+        Vessel v1 = new Vessel("V001", "Sea King", 25.5, "Cargo");
+        util.addVesselPerformance(v1);
+
+        System.out.println("Vessel added successfully");
+
     }
 }
